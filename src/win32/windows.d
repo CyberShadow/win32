a18807b6f9dc47b1e88a2e8d00b82586d676bf86
alexrp <xtzgzorex@gmail.com>
2012-05-22 23:32:37 +0200

    Use DWORD_PTR.

--- src/win32/windows.d
+++ src/win32/windows.d
@@ -97,6 +97,8 @@ else // Win32
     alias uint * PULONG_PTR;
 }
 
+    alias ULONG_PTR DWORD_PTR;
+
     alias void *HANDLE;
     alias void *PVOID;
     alias HANDLE HGLOBAL;
@@ -1376,7 +1378,7 @@ struct SYSTEM_INFO
     DWORD     dwPageSize;
     LPVOID    lpMinimumApplicationAddress;
     LPVOID    lpMaximumApplicationAddress;
-    ULONG_PTR dwActiveProcessorMask;
+    DWORD_PTR dwActiveProcessorMask;
     DWORD     dwNumberOfProcessors;
     DWORD     dwProcessorType;
     DWORD     dwAllocationGranularity;
