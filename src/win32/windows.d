CyberShadow/win32

commit 067120ddfb744e2c5624b217b58cb432e2493aad

Add version (Windows): to Windows headers.

src/win32/windows.d
@@ -16,8 +16,9 @@
 /// core.sys.windows.windows for the auto-generated win32 package.
 module win32.windows;
 
-
-extern (Windows) nothrow:
+//version (Windows):
+extern (Windows)
+nothrow:
 
     alias uint ULONG;
     alias ULONG *PULONG;
@@ -142,7 +143,7 @@ else // Win32
     alias DWORD   *LPCOLORREF;
     alias WORD    ATOM;
 
-version (0)
+version (none)
 {   // Properly prototyped versions
     alias BOOL function(HWND, UINT, WPARAM, LPARAM) DLGPROC;
     alias VOID function(HWND, UINT, UINT_PTR, DWORD) TIMERPROC;

commit bf41e447f39b0f29548807ecab2ed6f602ffddae

Fix a version statement broken in 067120ddfb744e2c5624b217b58cb432e2493aad.

src/win32/windows.d
@@ -143,7 +143,7 @@ else // Win32
     alias DWORD   *LPCOLORREF;
     alias WORD    ATOM;
 
-version (none)
+version (all)
 {   // Properly prototyped versions
     alias BOOL function(HWND, UINT, WPARAM, LPARAM) DLGPROC;
     alias VOID function(HWND, UINT, UINT_PTR, DWORD) TIMERPROC;